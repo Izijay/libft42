a9aeff9810d35bdc356cec929dc6573b627eadc7
Michael DUPUIS <mdupuis@e2r11p23.42.fr>
2020-12-08 21:32:20 +0100

    atoi cest pas facile

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mdupuis <mdupuis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 02:18:46 by mdupuis           #+#    #+#             */
-/*   Updated: 2020/12/08 18:26:15 by mdupuis          ###   ########.fr       */
+/*   Updated: 2020/12/08 21:31:12 by mdupuis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,47 +15,43 @@
 static int	ft_check_atoi(const char *str, int *i)
 {
 	int	sig;
-	int	neg;
 
-	sig = 0;
-	neg = 0;
+	sig = 1;
 	while ((str[*i] >= 9 && str[*i] <= 13) || str[*i] == 32)
 		*i = *i + 1;
-	while (str[*i] == '-' || str[*i] == '+')
+	if (str[*i] == '-' || str[*i] == '+')
 	{
 		if (str[*i] == '-')
-		{
-			sig++;
-			neg++;
-		}
-		else if (str[*i] == '+')
-			sig++;
+			sig = sig * -1;
 		*i = *i + 1;
 	}
-	if (sig > 1)
-		return (-1);
-	if (neg == 1)
-		return (1);
-	else
-		return (666);
+	return (sig);
 }
 
 int		ft_atoi(const char *str)
 {
 	long	res;
-	int		chk;
+	int		sig;
 	int		i;
+	int		ret;
 
 	i = 0;
 	res = 0;
-	chk = ft_check_atoi(str, &i);
+	ret = 1;
+	sig = ft_check_atoi(str, &i);
 	str += i;
-	if (chk == -1)
-		return (0);
 	while (*str && (*str >= '0' && *str <= '9'))
+	{
 		res = res * 10 + *str++ - 48;
-	if (chk == 1)
-		res = -res;
-	return (res);
+		if (res < 0 && (sig == 1 || sig == -1))
+		{
+			if (sig == 1)
+				ret = -1;
+			else
+				ret = 0;
+		}
+	}
+	if (ret == 1)
+		return (res * sig);
+	return (ret);
 }
-
